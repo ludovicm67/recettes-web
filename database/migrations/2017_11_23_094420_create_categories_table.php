ab00cb5f501dd69e63636966004fcfc9786179b3
Méline <meline.bl@outlook.fr>
2017-11-23 11:59:34 +0100

    Contraintes

--- database/migrations/2017_11_23_094420_create_categories_table.php
+++ database/migrations/2017_11_23_094420_create_categories_table.php
@@ -15,7 +15,7 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nom');
+            $table->string('nom')->unique();
             $table->timestamps();
         });
     }
