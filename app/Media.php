5758152f76d3afed813c51c3fb0e51e2c6d15e00
ludovicm67 <ludovicmuller1@gmail.com>
2017-11-30 12:51:49 +0100

    #3 added models

---
+++ app/Media.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Media extends Model
+{
+  /**
+   * The table associated with the model.
+   *
+   * @var string
+   */
+  protected $table = 'medias';
+  
+}
---
+++ app/Type.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Type extends Model
+{
+  /**
+   * The table associated with the model.
+   *
+   * @var string
+   */
+  protected $table = 'etape_types';
+  
+}
