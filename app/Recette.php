a71ba3a14e7dfd6c53874c2cbe520fca19c3a854
Méline <meline.bl@outlook.fr>
2017-12-02 13:05:33 +0100

    Recherche

--- app/Recette.php
+++ app/Recette.php
@@ -3,14 +3,43 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Nicolaslopezj\Searchable\SearchableTrait;
 
 class Recette extends Model
 {
+  use SearchableTrait;
+
+  /**
+   * Searchable rules.
+   *
+   * @var array
+   */
+   protected $searchable = [
+     /**
+     * Columns and their piority in search results.
+     * Colums with higher values are more important.
+     *
+     * @var array
+     */
+     'columns' => [
+       'recettes.nom' => 10,
+       'ingredients.nom' => 8,
+       'recettes.description' => 5
+     ],
+     'joins' => [
+       'ingredients_recette' => ['recettes.id', 'ingredients_recette.id_recettes'],
+       'ingredients' => ['ingredients.id', 'ingredients_recettes.id_ingredients']
+     ]
+   ];
+
+   //exemple de recherche :
+   //$recettes = Recette::search($query)->with('ingredients')->paginate(20);
+
   /**
    * The table associated with the model.
    *
    * @var string
    */
   protected $table = 'recettes';
-  
+
 }
