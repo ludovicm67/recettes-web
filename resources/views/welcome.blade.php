96f58d23e2c87cdd278c99a6cc7b4f11564a28f1
ludovicm67 <ludovicmuller1@gmail.com>
2017-12-06 18:24:07 +0100

    updated default vue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,93 +1,17 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Re7</title>
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">Bienvenue sur Re7</div>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        @if(Auth::user()->isAdmin())
-                          <a href="{{ route('users.index') }}">Utilisateurs</a>
-                        @endif
-                        <!-- <a href=" url('/home') ">Accueil</a> -->
-                        <!-- <a href="route('unites')">Unites</a> -->
-                        <a href="{{ route('recettes.index') }}">Recettes</a>
-                        <a href="{{ route('ingredients.index') }}">Ingredients</a>
-                    @else
-                        <a href="{{ route('login') }}">Se connecter</a>
-                        <a href="{{ route('register') }}">Créer un compte</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Re7
+                <div class="panel-body">
+                    <p>Découvrez de magnifiques recettes à préparer !</p>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+</div>
+@endsection
